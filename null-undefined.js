4204d161d8e337728eb01d9a3f98fa5d4511d6b0
sabbirromanbai <sabbirbai811@gmail.com>
2026-03-05 16:27:43 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,16 @@
+
+function add(a, b) {
+    console.log(a + b);
+}
+const result = add(10,);// undefined
+console.log(result);
+
+
+const person = {name: 'sagor', money: 5000};
+console.log(person.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let age = [24,25,26];
+console.log(age[11]);
