13e1adeb3b99b5b2d1aea14e2c3788db82fb9a72
sabbirromanbai <sabbirbai811@gmail.com>
2026-03-08 15:58:57 +0600

    almost done

---
+++ array-split.js
@@ -0,0 +1,8 @@
+const num = [1,2,3,4,5,6,7,8,9,];
+// const part = num.slice(2,5);
+const removed = num.splice(2,3,77,88);
+// console.log(removed);
+// console.log(part);
+// console.log(num);
+const together = num.join("ami");
+console.log(together);
